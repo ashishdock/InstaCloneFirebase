138ce589bbcb667a597a72fc6327b6b7109439d6
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-25 12:04:30 +0530

    SPM installed dependency Firebase Storage, showing error while compiling. Making anew branch.

--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 import FirebaseCore
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+    }
 }
 
